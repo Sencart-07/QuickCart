7d519db19dd8b7fcac9871133d5ad5814f19a8e2
sneha <modanwalsneha697@gmail.com>
2025-12-17 17:19:12 +0530

    First Commit

--- config/db.js
+++ config/db.js
@@ -1,29 +1,23 @@
-import mongoose from "mongoose";
-
-let cached = global.mongoose
 
 if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null }
+    cached = global.mongoose = { conn: null, Promise: null }
+
 }
 
 async function connectDB() {
+    
     if (cached.conn) {
         return cached.conn
     }
-
     if (!cached.promise) {
         const opts = {
-            bufferCommands: false
+            bufferCommands:false
         }
-
-        cached.promise = mongoose.connect('${process.env.MONGODB_URI}/quickcart', opts).then(mongoose => { 
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`,opts).then(mongoose => {
             return mongoose
         })
-    
     }
-
-    cached.conn= await cached.promise
+    cached.conn = await cached.promise
     return cached.conn
 }
-
 export default connectDB
